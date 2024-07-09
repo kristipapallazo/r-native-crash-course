68d9b3957c7bf7f2e06f265ce315ecd5ff62199a
Kristi Papallazo <papallazokristi@gmail.com>
2024-07-09 20:02:43 +0200

    initial_push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+//layout would be on every page (ex. a navbar)
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Slot } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <>
+      <Text>Header</Text>
+      <Slot />
+      <Text>Footer</Text>
+      {/* <View style={styles.container}>
+        <Text>RootLayout</Text>
+      </View> */}
+    </>
   );
-}
+};
+
+export default RootLayout;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     display: "flex",
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     padding: 20,
+//   },
+// });
